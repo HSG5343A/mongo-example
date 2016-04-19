ba132d1af283c6c87d9c700358e966fa41c19107
Darius <darius@artivest.co>
2016-04-19 17:39:50 -0400

    fix IP so it works locally

--- nyt.js
+++ nyt.js
@@ -65,7 +65,7 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/map.html');
 });
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 app.listen(port, ipaddress, function() {
     console.log('Example app listening on port ' + port);
